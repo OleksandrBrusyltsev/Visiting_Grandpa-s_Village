73d703cd5ff471451042ca339cd100fe2355d05c
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-03-24 10:11:08 +0200

    style: add sass

--- src/components/ui/icons/IconSocialNetwork.tsx
+++ src/components/ui/icons/IconSocialNetwork.tsx
@@ -1,37 +1,41 @@
-import { useState } from "react";
+import { useState, ReactNode } from "react";
 import styles from "./IconSocialNetwork.module.scss";
 
 type IconState = "default" | "hover" | "click" | "disable";
 
 interface IconProps {
   iconName: string;
+  children?: ReactNode;
   onClick?: () => void;
   isDisabled?: boolean;
 }
 
 const IconSocialNetwork: React.FC<IconProps> = ({
   iconName,
+  children,
   onClick,
   isDisabled,
 }) => {
   const [iconState, setIconState] = useState<IconState>("default");
 
-  const handleMouseEnter = () => setIconState("hover");
-  const handleMouseLeave = () => setIconState("default");
-  const handleMouseDown = () => setIconState("click");
-  const handleMouseUp = () => setIconState("hover");
+  const handleMouseEnter = () => !isDisabled && setIconState("hover");
+  const handleMouseLeave = () => !isDisabled && setIconState("default");
+  const handleMouseDown = () => !isDisabled && setIconState("click");
+  const handleMouseUp = () => !isDisabled && setIconState("hover");
 
   const className = `${styles.icon} ${styles[`${iconName}-${iconState}`]}`;
 
   return (
     <div
       className={className}
-      onClick={!isDisabled ? onClick : undefined}
-      onMouseEnter={!isDisabled ? handleMouseEnter : undefined}
-      onMouseLeave={!isDisabled ? handleMouseLeave : undefined}
-      onMouseDown={!isDisabled ? handleMouseDown : undefined}
-      onMouseUp={!isDisabled ? handleMouseUp : undefined}
-    />
+      onClick={onClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
+      {children}
+    </div>
   );
 };
 
