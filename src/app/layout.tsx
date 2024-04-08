69fece2ae3468f3707ef7cbdaff68d54e4d60f1d
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-04-08 15:39:02 +0300

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import Footer from "@/components/Footer/Footer";
-import "../styles/reset.css";
-import "../styles/variables.css";
 import "../styles/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body>
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-// import styles from "../styles/page.module.scss";
 import WelcomeBlock from "@/components/welcomeBlock/WelcomeBlock";
 
 export default function Home() {
-  return <main><WelcomeBlock/></main>;
+  return (
+    <main>
+      <WelcomeBlock />
+    </main>
+  );
 }
