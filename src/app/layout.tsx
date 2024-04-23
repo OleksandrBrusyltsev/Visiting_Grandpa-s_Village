a75eb80ad8c62d670673241432356d5757f8a5ee
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-04-23 16:24:46 +0300

    example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
-import Footer from "@/components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 import "../styles/globals.scss";
 
 // const inter = Inter({ subsets: ["latin"] });
