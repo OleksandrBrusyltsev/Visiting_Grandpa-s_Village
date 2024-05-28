d50c6e09c5ccaceb14dc80989660e576bfa0d781
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-28 14:16:42 +0300

    fix name and tel icon

--- src/components/ContactsComponent/TelegramBlock/TelegramBlock.tsx
+++ src/components/ContactsComponent/TelegramBlock/TelegramBlock.tsx
@@ -2,17 +2,16 @@
 
 import { FC } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import IconBack from "../../../assets/icons/icon-telegram.svg";
-import s from "./TelegramBotLink.module.scss";
+import Icon from "../../ui/Icon/Icon";
+import s from "./TelegramBlock.module.scss";
 
-const TelegramBotLink: FC = () => {
+const TelegramBlock: FC = () => {
   return (
     <div className={s.telegramWrapper}>
       <h1 className={s.telegramTitle}>Зв’язатись з Дідусем</h1>
       <p className={s.telegramText}>
         Незважаючи на поважний вік твій Дідусь дуже сучасний, тому має
-        <span className={s.telegramTextBolt}>&#x20;Telegram-bot&#x20;</span>,
+        <span className={s.telegramTextBold}>&#x20;Telegram-bot&#x20;</span>,
         знаю як ти любиш корисні зручні сервіси.
       </p>
       <div className={s.telegramLinkWrapper}>
@@ -20,7 +19,7 @@ const TelegramBotLink: FC = () => {
           Скористайся, щоб забронювати будиночок, або запитати чат-бот
         </p>
         <Link href="#" className={s.telegramBotLink}>
-          <Image src={IconBack} alt="back" className={s.telegramIcon} />
+          <Icon name="telegram" className={s.telegramIcon} />
           <p className={s.telegramIconText}>Чат-Бот</p>
         </Link>
       </div>
@@ -28,4 +27,4 @@ const TelegramBotLink: FC = () => {
   );
 };
 
-export default TelegramBotLink;
+export default TelegramBlock;
