a045bc7b83c9accc63f0ae3f22e2d9889a38c3a5
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-31 01:38:13 +0300

    add RouteInfo

--- src/components/ContactsComponent/ContactsComponent.tsx
+++ src/components/ContactsComponent/ContactsComponent.tsx
@@ -6,10 +6,13 @@ import IconBack from "../../assets/icons/icon-back.svg";
 import TelegramBotLink from "./TelegramBlock/TelegramBlock";
 import ContactsList from "./ContactsList/ContactsList";
 import FAQ from "./FAQ/FAQ";
+import RouteInfo from "./RouteInfo/RouteInfo";
+import GoogleMap from "../../../public/images/contacts/google-map.jpg";
 import s from "./ContactsComponent.module.scss";
 
-const ContactsComponent:FC = () => {
-    return (
+const ContactsComponent: FC = () => {
+  return (
+    <>
       <div className={s.contactsContainer}>
         <div className={s.navWrapper}>
           <div className={s.linkTextWrapper}>
@@ -18,13 +21,18 @@ const ContactsComponent:FC = () => {
           </div>
           <Link href="/" className={s.backLink}>
             <Image src={IconBack} alt="back" className={s.backIcon} />
-                </Link>
-            </div>
+          </Link>
+        </div>
         <TelegramBotLink />
         <ContactsList />
-        <FAQ/>
+        <FAQ />
+        <RouteInfo/>
       </div>
-    );
+      <Link href="https://www.google.com/maps/place/%D0%9D%D0%B0+%D1%81%D0%B5%D0%BB%D1%96+%D1%83+%D0%94%D1%96%D0%B4%D1%83%D1%81%D1%8F/@51.9628026,31.1596513,17z/data=!3m1!4b1!4m9!3m8!1s0x46d502a972adef5f:0xd2021628aff7a3c7!5m2!4m1!1i2!8m2!3d51.9628026!4d31.1622262!16s%2Fg%2F11g8ph1fsl?authuser=0&entry=ttu">
+        <Image className={s.googleMap} src={GoogleMap} alt="google-map" />
+      </Link>
+    </>
+  );
 };
 
 export default ContactsComponent;
---
+++ src/components/ContactsComponent/RouteInfo/RouteInfo.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import Image from "next/image";
+import House from "../../../../public/images/contacts/house.png";
+import s from "./RouteInfo.module.scss";
+
+const RouteInfo: FC = () => {
+  return (
+    <div className={s.imgAndRouteInfoWrapper}>
+      <Image src={House} alt="picture" className={s.house} />
+      <h1 className={s.routeInfoTitle}>Як дістатися з Чернігова?</h1>
+      <div className={s.routeInfoWrapper}>
+        <p className={s.routeInfoText}>
+          Електричкою до станції Грибова Рудня. Електричка виходить із Чернігова
+          до Грибової Рудні двічі на день — о 8:16 ранку та о 21:20 вечора
+        </p>
+        <p className={s.routeInfoText}>
+          Назад: із Грибової Рудні до Чернігова о 15:20 (1 раз на день) Автобус
+          ходить один раз на день Чернігів-Ріпки-Олешня Виїзд орієнтовно о 12.30
+          від автостанції «Нива» Чернігів Виїзд з Олешні орієнтовно о 17:20.
+        </p>
+        <div className={s.textDotWrapper}>
+          <span className={s.dot}></span>
+          <p className={s.textDot}>інформація може змінюватись</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RouteInfo;
