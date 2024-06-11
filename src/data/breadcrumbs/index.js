c042e4eefb46dbdc42c17d8cab463cb565e6d8cf
Homer <Home@fdasdar>
2024-06-11 21:19:53 +0300

    added data file

---
+++ src/data/breadcrumbs/index.js
@@ -0,0 +1,52 @@
+export const breadcrumbs = [
+    {
+      id: 0,
+      link: "",
+      text: "ГОЛОВНА",
+    },
+    {
+      id: 1,
+      link: "houses",
+      text: "ЖИТИ",
+      subNav: 'custom'
+    },
+    { id: 2, link: "", text: "ЇСТИ" },
+    {
+      id: 3,
+      link: "",
+      text: "БАЙДИКУВАТИ",
+    },
+    {
+      id: 4,
+      link: "memories",
+      text: "СПОГАДИ",
+      subNav: 'custom'
+    },
+    {
+      id: 5,
+      link: "contacts",
+      text: "ЗНАЙТИ МЕНЕ",
+    },
+    {
+      id: 6,
+      link: "booking",
+      text: "ЗАВІТАТИ",
+      subNav: [
+        {
+          id: 1,
+          link: "options",
+          text: "варіанти бронювань",
+        },
+        {
+          id: 2,
+          link: "payment",
+          text: "оплата",
+        },
+        {
+          id: 3,
+          link: "rules",
+          text: "Умови бронювання та правила перебування",
+        }
+      ]
+    },
+  ];
