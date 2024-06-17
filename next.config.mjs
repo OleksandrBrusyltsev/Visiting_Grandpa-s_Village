7fa11e5f4c1f7a5b1fe5df54ef5592ba5be30688
Babenko Liudmyla <savelieva0509@gmail.com>
2024-06-17 21:22:21 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,5 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-          {
-            destination: "/:locale/contacts",
-            source: "/:locale/contacts",
-          },
-        ];
-    }
-}
+const nextConfig = {};
 export default withNextIntl(nextConfig);
-
-
