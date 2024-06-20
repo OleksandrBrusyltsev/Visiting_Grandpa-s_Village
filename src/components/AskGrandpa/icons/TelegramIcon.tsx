5fee5a312fbb58f16033eb822d42d2015dad2ac5
Homer <Home@fdasdar>
2024-06-20 11:34:15 +0300

    removed unnecessary files askgrandpa

--- src/components/AskGrandpa/icons/TelegramIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import telegramIcon from "../../../assets/icons/askGrandpa/telegramIcon";
-import style from "../AskGrandpa.module.scss";
-
-const TelegramIcon: FC = () => {
-  return <div className={style.iconTelegram}>{telegramIcon}</div>;
-};
-
-export default TelegramIcon;
