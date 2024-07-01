715384d32439b399182ed444e0f6f4c72f6c7ce1
BerLP2021 <94077466+BerLP2021@users.noreply.github.com>
2024-07-01 22:39:21 +0300

    Feat/add hero (#59)
    * add hero to mobile
    
    * заменил картинку стрелки на свг
    
    * верстка под мобилку
    
    заменил фото на png, изменил размеры фото, т.к. оказалось в новом дизайне новые размеры
    
    * герой(моб версия)
    
    где-то пробивает ширину, не могу найти((
    
    * новая верстка на мобилку
    
    * пофиксил резиновую верстку от 375 до 575
    
    * адаптив под 768
    
    * адаптив десктоп
    
    * add hero to mobile
    
    * заменил картинку стрелки на свг
    
    * верстка под мобилку
    
    заменил фото на png, изменил размеры фото, т.к. оказалось в новом дизайне новые размеры
    
    * герой(моб версия)
    
    где-то пробивает ширину, не могу найти((
    
    * новая верстка на мобилку
    
    * пофиксил резиновую верстку от 375 до 575
    
    * адаптив под 768
    
    * адаптив десктоп
    
    * upl new photo, add calendar to hero, fix some bugs
    
    * fix img grandpa2
    
    добавил маску с засветом, что бы было видно текст сверху
    
    * add animation to mobile
    
    * fix mini bugs
    
    * удалил ненужный импорт
    
    может решить ошибку 1 при деплое
    
    * add animation to navLinks
    
    * mini fix
    
    * фикс для билда
    
    * add scrollAnimation + animation on desktop
    
    * markup fix
    
    * тест
    
    * q
    
    * замена на ссылку
    
    * фикс длительности анимации
    
    * fix animation at Hero
    
    * замена фото на ссылки из клоудинари
    
    * замена фотки
    
    * мини фикс
    
    * фикс длительность анимации вверху страницы
    
    * 12
    
    * Merge branch 'develop' into feat/add-hero
    
    * fix font
    
    ---------
    
    Co-authored-by: Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 import { getLocale } from "next-intl/server";
 
@@ -6,7 +6,7 @@ import GalleryItem from "./GalleryItem";
 import Button from "../ui/Button/Button";
 import Icon from "../ui/Icon/Icon";
 
-import s from './Gallery.module.scss';
+import s from "./Gallery.module.scss";
 
 type Props = {items: GalleryItem[]}
 
@@ -16,13 +16,17 @@ export default async function Gallery({items}: Props) {
     <div className={s.backgroundImages}>
       <section className={s.hero}>
         <div className={s.heroWrapper}>
-          <p className={s.descr1}>Люблю згадувати всі щасливі моменти, <span className={s.noBreak}>що відбулись</span> <span className={s.noBreak}>“На Селі у Дудуся”</span></p>
+          <p className={s.descr1}>
+            Люблю згадувати всі щасливі моменти,{" "}
+            <span className={s.noBreak}>що відбулись</span>{" "}
+            <span className={s.noBreak}>“На Селі у Дудуся”</span>
+          </p>
           <p className={s.descr2}>Згадаєш зі мною?</p>
           <div className={s.grandpa}>
-            <Image 
+            <Image
               fill
-              alt="Friendly Grandpa" 
-              src="/images/grandpas/Grandpa1.png" 
+              alt="Friendly Grandpa"
+              src="/images/grandpas/Grandpa1.png"
               sizes="(max-width: 768px) 100vw, 50vw"
             />
           </div>
@@ -30,7 +34,11 @@ export default async function Gallery({items}: Props) {
       </section>
       <main className={s.main}>
         <section className={s.memoriesGallery}>
-          <h1 className={s.callToClick}><span className={s.desktopOnly}>Клікай</span><span className={s.mobileOnly}>Натискай</span> на фото, щоб подивитись більше</h1>
+          <h1 className={s.callToClick}>
+            <span className={s.desktopOnly}>Клікай</span>
+            <span className={s.mobileOnly}>Натискай</span> на фото, щоб
+            подивитись більше
+          </h1>
           <div className={s.galleryWrapper}>
             {items.map((item, i) => (<GalleryItem data={item} key={i}/>))}
           </div>
@@ -39,9 +47,11 @@ export default async function Gallery({items}: Props) {
           <div className={s.cloud}>
             <Icon name="cloud" />
           </div>
-          <p className={s.slogan}>А далі створимо нові щасливі спогади разом.</p>
+          <p className={s.slogan}>
+            А далі створимо нові щасливі спогади разом.
+          </p>
           <Link href={`/${locale}/booking`}>
-            <Button 
+            <Button
               label="Завітати"
               type={"button"}
               className={s.btnCallToAction}
@@ -50,5 +60,5 @@ export default async function Gallery({items}: Props) {
         </div>
       </main>
     </div>
-  )
+  );
 }
