359c54be708eb9ce7cdf3797d02a94339932512f
Babenko Liudmyla <savelieva0509@gmail.com>
2024-07-02 23:11:36 +0300

    fix map

--- src/components/ContactsComponent/ContactsComponent.tsx
+++ src/components/ContactsComponent/ContactsComponent.tsx
@@ -8,11 +8,11 @@ import TelegramBotLink from "./TelegramBlock/TelegramBlock";
 import ContactsList from "./ContactsList/ContactsList";
 import FAQ from "./FAQ/FAQ";
 import RouteInfo from "./RouteInfo/RouteInfo";
+import GoogleMap from "./GoogleMap/GoogleMap";
 import s from "./ContactsComponent.module.scss";
 
 const ContactsComponent: FC = () => {
   const googleMapWrapper = useRef<HTMLDivElement>(null);
-  const googleMap = useRef<HTMLDivElement>(null);
   gsap.registerPlugin(ScrollTrigger);
 
   useGSAP(() => {
@@ -44,16 +44,7 @@ const ContactsComponent: FC = () => {
         <RouteInfo />
       </div>
       <div className={s.googleMapWrapper} ref={googleMapWrapper}>
-        <iframe
-          className={s.googleMap}
-          width="100%"
-          height="600"
-          frameBorder="0"
-          scrolling="no"
-          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F%20%D0%9F%D0%B5%D1%80%D1%88%D0%BE%D1%82%D1%80%D0%B0%D0%B2%D0%BD%D0%B5%D0%B2%D0%B0,%205,%20%D0%9E%D0%BB%D0%B5%D1%88%D0%BD%D1%8F,%20%D0%A7%D0%B5%D1%80%D0%BD%D1%96%D0%B3%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0%20%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,%2015030+(Grandpa's%20Village)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-        >
-          <a href="https://www.gps.ie/">gps systems</a>
-        </iframe>
+        <GoogleMap />
       </div>
     </>
   );
---
+++ src/components/ContactsComponent/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import {
+  APIProvider,
+  Map,
+   AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+
+const GoogleMap = () => {
+  const position = { lat: 51.96278, lng: 31.1626 };
+  const apiKey =
+    process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "YOUR_DEFAULT_API_KEY";
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <APIProvider apiKey={apiKey}>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <Map
+          defaultCenter={position}
+          zoom={17}
+          center={position}
+          mapId={process.env.NEXT_PUBLIC_MAP_ADI}
+        >
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin
+              background={"rgb(194, 191, 183)"}
+              borderColor={"rgb( 97, 97, 97)"}
+              glyphColor={"rgb(63, 85, 64)"}
+              scale={2}
+            ></Pin>
+          </AdvancedMarker>
+
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <p>Welcome to sadyba!</p>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
+  );
+};
+
+export default GoogleMap;
