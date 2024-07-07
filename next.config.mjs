d74b2796f1a67363b0717d5a074341f0821737ea
Babenko Liudmyla <savelieva0509@gmail.com>
2024-07-07 18:03:35 +0300

    fix scroll

--- next.config.mjs
+++ next.config.mjs
@@ -18,8 +18,7 @@ const nextConfig = {
     ],
   },
   env: {
-    GOOGLE_MAPS_API_KEY:
-      process.env.GOOGLE_MAPS_API_KEY,
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
     MAP_ADI: process.env.MAP_ADI,
   },
 };
