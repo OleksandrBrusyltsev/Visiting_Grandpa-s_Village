2ccedcbb2a2de2b51c85970e3f21b5ac06b8333b
Babenko Liudmyla <savelieva0509@gmail.com>
2024-07-09 16:51:21 +0300

    Update FAQ.tsx

--- src/components/ContactsComponent/FAQ/FAQ.tsx
+++ src/components/ContactsComponent/FAQ/FAQ.tsx
@@ -19,54 +19,51 @@ const FAQ: FC = () => {
   const imgAndFaqWrapper = useRef<HTMLDivElement>(null);
   const answerRefs = useRef<(HTMLParagraphElement | null)[]>([]);
 
-  useGSAP(() => {
-    gsap.fromTo(
-      lake.current,
-      {
-        x: "-100%",
-      },
-      {
-        scrollTrigger: {
-          trigger: lake.current,
-        },
-        x: "0%",
-        duration: 1,
-        clearProps: "transform",
-      }
-    );
-    gsap.fromTo(
-      faqTitle.current,
-      {
-        x: "100%",
-      },
-      {
-        scrollTrigger: {
-          trigger: faqTitle.current,
-        },
-        x: "0%",
-        duration: 1,
-        clearProps: "transform",
-      }
-    );
-    gsap.fromTo(
-      faqWrapper.current,
-      {
-        x: "100%",
-        autoAlpha: 0,
-      },
-      {
-        scrollTrigger: {
-          trigger: faqWrapper.current,
-          markers: true,
-        },
-        x: "0%",
-        autoAlpha: 1,
-        duration: 1,
-        clearProps: "transform,opacity,visibility",
-        onComplete: () => ScrollTrigger.refresh(),
-      }
-    );
-  });
+ useGSAP(() => {
+   gsap.fromTo(
+     lake.current,
+     {
+       x: "-100%",
+     },
+     {
+       scrollTrigger: {
+         trigger: lake.current,
+       },
+       x: "0%",
+       duration: 1,
+       clearProps: "transform",
+     }
+   );
+   gsap.fromTo(
+     faqTitle.current,
+     {
+       x: "100%",
+     },
+     {
+       scrollTrigger: {
+         trigger: faqTitle.current,
+       },
+       x: "0%",
+       duration: 1,
+       clearProps: "transform",
+     }
+   );
+   gsap.fromTo(
+     faqWrapper.current,
+     {
+       x: "100%",
+     },
+     {
+       scrollTrigger: {
+         trigger: faqWrapper.current,
+       },
+       x: "0%",
+       duration: 1,
+       clearProps: "transform",
+     }
+   );
+ });
+
 
   const toggleAnswer = (index: number) => {
     if (openIndices.includes(index)) {
