8184e4fd5552c94ad247fb9f9fad2a542508344e
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2024-07-22 13:11:14 +0300

    fix navigation #81

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -6,16 +6,22 @@ import { useLocale } from "next-intl";
 import css from "./Navigation.module.scss";
 
 import { navigationLinks } from "@/data/header/navigationData";
+import { usePathname } from "next/navigation";
+
 
 
 const Navigation = () => {
   const locale = useLocale();
+  const pathname = usePathname()
 
   return (
     <div className={css.container}>
       <ul className={css.list}>
         {navigationLinks.map(({ id, link, text }) => (
-          <li key={id} className={css.item}>
+          <li
+            key={id}
+            className={`${css.item} ${pathname === `/${locale}/${link}` ? css.active : ''}`}
+          >
             <Link href={`/${locale}/${link}`}>{text}</Link>
           </li>
         ))}
