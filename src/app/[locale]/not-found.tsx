cce418a15d6a41782ab715cd6898e48c8897b337
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2024-08-05 22:54:33 +0300

    12345

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,6 +1,13 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import NotFound from "@/components/NotFound/NotFound";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Сторінка не знайдена - 404",
+  description:
+    "Сторінка, яку ви шукаєте, не знайдена. Поверніться на головну сторінку або скористайтеся навігацією.",
+};
 
 export default function NotFoundPage() {
   return   (
