c503fbb1167a5900db2f850dd0f46450b10c3b3e
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2024-08-05 21:47:14 +0300

    555

--- src/components/ContactsComponent/ContactsComponent.tsx
+++ src/components/ContactsComponent/ContactsComponent.tsx
@@ -8,6 +8,7 @@ import FAQ from "./FAQ/FAQ";
 import RouteInfo from "./RouteInfo/RouteInfo";
 import GoogleMap from "./GoogleMap/GoogleMap";
 import s from "./ContactsComponent.module.scss";
+import { ScrollTrigger } from "gsap/all";
 
 const ContactsComponent: FC = () => {
   // Create refs for TelegramBlock elements
@@ -32,137 +33,137 @@ const ContactsComponent: FC = () => {
   // Create refs for googleMapWrapper element
   const googleMapWrapper = useRef<HTMLDivElement>(null);
 
-  // useGSAP(() => {
+  useGSAP(() => {
 
-  //   // TelegramBlock animations
-  //   gsap.timeline({defaults: {
-  //     x: -200, 
-  //     opacity: 0,
-  //     duration: 0.7
-  //   }})
-  //     .from(telegramTitle.current, {})
-  //     .from(telegramText.current, {}, ">-0.5")
-  //     .from(map.current, {
-  //       x: 200,
-  //       opacity: 0,
-  //       duration: 1
-  //     }, 0)
-  //     .from(telegramLinkWrapper.current, {}, ">-0.5")
+    // TelegramBlock animations
+    gsap.timeline({defaults: {
+      x: -200, 
+      opacity: 0,
+      duration: 0.7
+    }})
+      .from(telegramTitle.current, {})
+      .from(telegramText.current, {}, ">-0.5")
+      .from(map.current, {
+        x: 200,
+        opacity: 0,
+        duration: 1
+      }, 0)
+      .from(telegramLinkWrapper.current, {}, ">-0.5")
     
-  //   // contactsList animation
-  //   gsap.timeline({
-  //     defaults: {
-  //       duration: 0.5,
-  //       opacity: 0,
-  //     },
-  //     scrollTrigger: {
-  //       trigger: contactsListWrapper.current,
-  //       start: "top 80%",
-  //     }
-  //   })
-  //     .from(grandpa.current, {
-  //       scale: 0.9,
-  //       y: -100,
-  //     })
-  //     .from(contactsListWrapper.current, {
-  //       clipPath: "inset(0% 0% 100% 0%)",
-  //       duration: 0.7
-  //     })
+    // contactsList animation
+    gsap.timeline({
+      defaults: {
+        duration: 0.5,
+        opacity: 0,
+      },
+      scrollTrigger: {
+        trigger: contactsListWrapper.current,
+        start: "top 80%",
+      }
+    })
+      .from(grandpa.current, {
+        scale: 0.9,
+        y: -100,
+      })
+      .from(contactsListWrapper.current, {
+        clipPath: "inset(0% 0% 100% 0%)",
+        duration: 0.7
+      })
       
-  //   const mm = gsap.matchMedia();
-  //   mm.add(
-  //     {
-  //       isMobile: "(max-width: 999px)",
-  //       isNotMobile: "(min-width: 1000px)",
-  //     },
-  //     (context) => {
-  //       const { isMobile, isNotMobile } = context.conditions as gsap.Conditions;
+    const mm = gsap.matchMedia();
+    mm.add(
+      {
+        isMobile: "(max-width: 999px)",
+        isNotMobile: "(min-width: 1000px)",
+      },
+      (context) => {
+        const { isMobile, isNotMobile } = context.conditions as gsap.Conditions;
 
-  //       if (isMobile) {
-  //         // FAQ animation
-  //         gsap.from(lake.current, {
-  //           x: -200,
-  //           opacity: 0,
-  //           duration: 0.7,
-  //           scrollTrigger: {
-  //             trigger:  lake.current,
-  //             start: "top 80%",
-  //           }
-  //         })
+        if (isMobile) {
+          // FAQ animation
+          gsap.from(lake.current, {
+            x: -200,
+            opacity: 0,
+            duration: 0.7,
+            scrollTrigger: {
+              trigger:  lake.current,
+              start: "top 80%",
+            }
+          })
 
-  //         gsap.timeline({
-  //           defaults: {
-  //             opacity: 0,
-  //             duration: 0.7,
-  //           },
-  //           scrollTrigger: {
-  //             trigger: faqTitle.current,
-  //             start: "top 80%",
-  //           }
-  //         })
-  //           .from(faqTitle.current, {y: -100})
-  //           .from(faqWrapper.current, {
-  //             y: -100,
-  //           }, ">-0.3");
-  //       }
+          gsap.timeline({
+            defaults: {
+              opacity: 0,
+              duration: 0.7,
+            },
+            scrollTrigger: {
+              trigger: faqTitle.current,
+              start: "top 80%",
+            }
+          })
+            .from(faqTitle.current, {y: -100})
+            .from(faqWrapper.current, {
+              y: -100,
+            }, ">-0.3");
+        }
 
-  //       if (isNotMobile) {
-  //         // FAQ animation
-  //         gsap.timeline({
-  //           defaults: {
-  //             opacity: 0,
-  //             duration: 0.7,
-  //           },
-  //           scrollTrigger: {
-  //             trigger:  lake.current,
-  //             start: "top 80%",
-  //           }
-  //         })
-  //           .from(lake.current, {x: -200})
-  //           .from(faqTitle.current, {
-  //             x: 200,
-  //           }, "<")
-  //           .from(faqWrapper.current, {
-  //             x: 200,
-  //           }, ">-0.3");
-  //       }
-  //     }
-  //   );
+        if (isNotMobile) {
+          // FAQ animation
+          gsap.timeline({
+            defaults: {
+              opacity: 0,
+              duration: 0.7,
+            },
+            scrollTrigger: {
+              trigger:  lake.current,
+              start: "top 80%",
+            }
+          })
+            .from(lake.current, {x: -200})
+            .from(faqTitle.current, {
+              x: 200,
+            }, "<")
+            .from(faqWrapper.current, {
+              x: 200,
+            }, ">-0.3");
+        }
+      }
+    );
     
-  //   // RouteInfo animation
-  //   gsap.from(house.current, {
-  //     x: 200,
-  //     opacity: 0,
-  //     duration: 0.7,
-  //     scrollTrigger: {
-  //       trigger: house.current,
-  //       start: "top 80%",
-  //     }
-  //   });
+    // RouteInfo animation
+    gsap.from(house.current, {
+      x: 200,
+      opacity: 0,
+      duration: 0.7,
+      scrollTrigger: {
+        trigger: house.current,
+        start: "top 80%",
+      }
+    });
 
-  //   gsap.from(routeInfoWrapper.current, {
-  //     x: -100,
-  //     opacity: 0,
-  //     duration: 0.7,
-  //     scrollTrigger: {
-  //       trigger: routeInfoWrapper.current,
-  //       start: "top 80%",
-  //     }
-  //   });
+    gsap.from(routeInfoWrapper.current, {
+      x: -100,
+      opacity: 0,
+      duration: 0.7,
+      scrollTrigger: {
+        trigger: routeInfoWrapper.current,
+        start: "top 80%",
+      }
+    });
     
-  //   // GoogleMap animation
-  //   // gsap.from(googleMapWrapper.current, {
-  //   //   y: 200,
-  //   //   autoAlpha: 0,
-  //   //   duration: 0.7,
-  //   //   scrollTrigger: {
-  //   //     trigger: googleMapWrapper.current,
-  //   //     start: "top 80%",
-  //   //   },
-  //   //   clearProps: "transform",
-  //   // });
-
-  // });
+    // GoogleMap animation
+    gsap.from(googleMapWrapper.current, {
+      y: 200,
+      autoAlpha: 0,
+      duration: 0.7,
+      scrollTrigger: {
+        trigger: googleMapWrapper.current,
+        start: "top 80%",
+      },
+      clearProps: "transform",
+    });
+    ScrollTrigger.refresh(true);
+  });
 
   return (
     <>
