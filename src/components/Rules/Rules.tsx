cbbef5a1a82126b0f084a676e942c1f10a0445a7
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-10 19:04:11 +0300

    add some styles

--- src/components/Rules/Rules.tsx
+++ src/components/Rules/Rules.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import Image from "next/image";
 import Granpa1 from "../../../public/images/grandpas/Grandpa5.png";
 import Grandpa2 from "../../../public/images/grandpas/Grandpa6.png";
+import IconErrow from "../../assets/icons/icon-arrow-rules.svg";
 import { rules } from "../../data/rules/index.js";
 import s from "./Rules.module.scss";
 
@@ -15,9 +16,10 @@ const Rules: FC = () => {
         </h1>
         <Image src={Granpa1} alt="picture" className={s.rulesTitleImg}></Image>
       </div>
-      <ul className={s.rulesList}>
+      <Image src={IconErrow} alt="arrow" className={s.iconErrow} />
+      <ul className={s.ruleslist}>
         {rules.map((rule) => (
-          <li key={rule.id} className={s.ruleItem}>
+          <li key={rule.id} className={s.rulesItem}>
             <h3 className={s.ruleTitle}>{rule.title}</h3>
             <p
               className={s.ruleDescription}
@@ -27,23 +29,19 @@ const Rules: FC = () => {
         ))}
       </ul>
       <div className={s.rulesRememberWrapper}>
+        <Image src={Grandpa2} alt="picture" className={s.itemImg} />
         <ul className={s.rulesRememberList}>
-          <Image
-            src={Grandpa2}
-            alt="picture"
-            className={s.rulesRememberImg}
-          ></Image>
-          <li className={s.rulesRememberItem}>
-            Невеличке нагадування: Дідусь та команда Еко-садиби “На селі у
-            Дідуся” - твої друзі, завжди готові допомогти та зробити відпочинок
-            приємним і незабутнім.
+          <li className={s.rulesRememberItemFirst}>
+            <u>Невеличке нагадування:</u>&nbsp; Дідусь та команда Еко-садиби “На
+            селі у Дідуся” - твої друзі, завжди готові допомогти та зробити
+            відпочинок приємним і незабутнім.
           </li>
-          <li>
+          <li className={s.rulesRememberItem}>
             Також пам’ятайте, що ми не можемо контролювати погоду та інші
             зовнішні обставини, проте гарантуємо вам теплий прийом та комфортний
             відпочинок.
           </li>
-          <li>
+          <li className={s.rulesRememberItem}>
             “На селі у Дідуся” гості отримують гарні, приємні емоції та
             створюють неповторні, теплі спогади.
           </li>
