5a28b413a7eea541795a727d3e7a5a390ead1c01
olegtkach <olegontkach101@gmail.com>
2024-08-16 08:40:30 +0300

    fix: locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
-  locales: ["uk", "en"],
+  locales: ["uk",],
   defaultLocale: "uk",
   // localeDetection: true,
 });
 
 export const config = {
-  matcher: ["/", "/(en|uk)/:path*"],
+  matcher: ["/", "/(uk)/:path*"],
 };
