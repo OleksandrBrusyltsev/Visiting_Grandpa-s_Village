1e2ad1cf29e0da2b0f8c59f278c1ffb207426972
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2024-08-20 20:34:58 +0300

    Update GoogleMap.tsx

--- src/components/ContactsComponent/GoogleMap/GoogleMap.tsx
+++ src/components/ContactsComponent/GoogleMap/GoogleMap.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState, FC } from "react";
+import { useState, FC } from "react";
 import {
   APIProvider,
   Map,
   AdvancedMarker,
+  Pin,
   InfoWindow,
 } from "@vis.gl/react-google-maps";
 import s from "./GoogleMap.module.scss";
 import Image from "next/image";
 import grandpa from '../../../../public/images/grandpas/Grandpa1.png';
 import { CustomMarker } from "./CustomMarker";
-
 type GoogleMapProps = {
   googleMapWrapperRef: React.RefObject<HTMLDivElement>;
 };
@@ -18,46 +18,37 @@ const GoogleMap: FC<GoogleMapProps> = ({ googleMapWrapperRef }) => {
   const position = { lat: 51.96278, lng: 31.1626 };
   const apiKey =
     process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "YOUR_DEFAULT_API_KEY";
-
-  const [isLoaded, setIsLoaded] = useState(false);
   const [open, setOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    setTimeout(() => setIsLoaded(true), 500);
-  }, []);
 
   return (
     <APIProvider apiKey={apiKey}>
       <div
         ref={googleMapWrapperRef}
-        className={`${s.googleMapWrapper} ${isLoaded ? s.mapLoaded : ""}`} // Добавляем класс для анимации после загрузки
+        className={s.googleMapWrapper}
       >
-        {isLoaded ? (
-          <Map
-            defaultCenter={position}
-            defaultZoom={17}
-            mapId={process.env.NEXT_PUBLIC_MAP_ADI}
-          >
-            <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-              <CustomMarker />
-            </AdvancedMarker>
+        <Map
+          defaultCenter={position}
+          defaultZoom={17}
+          mapId={process.env.NEXT_PUBLIC_MAP_ADI}
+        >
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            {/* <Pin
+              background={"rgb(194, 191, 183)"}
+              borderColor={"rgb( 97, 97, 97)"}
+              glyphColor={"rgb(63, 85, 64)"}
+              scale={2}
+            ></Pin> */}
+            <CustomMarker />
+          </AdvancedMarker>
 
-            {open && (
-              <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-                <Image
-                  src={grandpa}
-                  alt="grandpa"
-                  width={75}
-                  height={75}
-                  style={{ transform: 'translateX(50%)' }}
-                />
-                <p style={{ fontSize: 16 }}>Welcome to Sadyba!</p>
-              </InfoWindow>
-            )}
-          </Map>
-        ) : (
-          <p>Loading map...</p> //
-        )}
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <Image src={grandpa} alt="grandpa" width={75} height={75} style={{transform: 'translateX(50%)'}}/>
+              <p style={{fontSize: 16}}>Welcome to Sadyba!</p>
+
+            </InfoWindow>
+          )}
+        </Map>
       </div>
     </APIProvider>
   );
