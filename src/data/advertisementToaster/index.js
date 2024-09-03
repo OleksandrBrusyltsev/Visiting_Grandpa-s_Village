b7165b2043ae88bb9072760a6793e05822d157c5
Homer <Home@fdasdar>
2024-09-03 12:05:38 +0300

    added mocks for toaster

---
+++ src/data/advertisementToaster/index.js
@@ -0,0 +1,65 @@
+export const toasters = [
+    {
+        id: 1,
+        //допустимые виды разделителей (в любой комбинации): ./,-
+        startDate: '01.08.2024',
+        endDate: '30.08.2024', //включая последний день
+        timeout: 0.33, // в минутах
+        translations: [
+            {
+                language: 'ru',
+                text: 'Старая акция',
+            },
+            {
+                language: 'en',
+                text: 'Past promotion',
+            },
+            {
+                language: 'uk',
+                text: 'Стара акція',
+            },
+        ],
+    },
+    {
+        id: 2,
+        //допустимые виды разделителей (в любой комбинации): ./,-
+        startDate: '01.09.2024',
+        endDate: '30.09.2024', //включая последний день
+        timeout: 0.33, // в минутах
+        translations: [
+            {
+                language: 'ru',
+                text: 'Дедушка советует! Забронируй любой домик со специальной скидкой 10% с 15 сентября! Успей воспользоваться уникальным предложением',
+            },
+            {
+                language: 'en',
+                text: 'Grandpa advises! Book any house with a special 10% discount up to 15th of September! Take advantage of the unique offer',
+            },
+            {
+                language: 'uk',
+                text: 'Дідусь радить! Забронюй будь-який будиночок зі спеціальною знижкою 10% з 15 вересня ! Встигни скористатись унікальною пропозицією',
+            },
+        ],
+    },
+    {
+        id: 3,
+        //допустимые виды разделителей (в любой комбинации): ./,-
+        startDate: '01.10.2024',
+        endDate: '31.10.2024', //включая последний день
+        timeout: 0.33, // в минутах
+        translations: [
+            {
+                language: 'ru',
+                text: 'Будущая акция',
+            },
+            {
+                language: 'en',
+                text: 'future promotion',
+            },
+            {
+                language: 'uk',
+                text: 'Майбутня акція',
+            },
+        ],
+    },
+];
