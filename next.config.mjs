63761f4c866e866ed616fea3c789eea4a1a1e19f
Alex <brusiltsev1@gmail.com>
2024-09-03 20:05:38 +0300

    dis

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
-import createNextIntlPlugin from "next-intl/plugin";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const withNextIntl = createNextIntlPlugin();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    formats: ["image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-      },
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
-    process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_MAP_ADI: process.env.NEXT_PUBLIC_MAP_ADI,
-  },
-};
-
-export default withNextIntl(nextConfig);
+// import createNextIntlPlugin from "next-intl/plugin";
+// import dotenv from "dotenv";
+//
+// dotenv.config();
+//
+// const withNextIntl = createNextIntlPlugin();
+//
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     formats: ["image/webp"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//         port: "",
+//       },
+//     ],
+//   },
+//   env: {
+//     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+//     process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+//     NEXT_PUBLIC_MAP_ADI: process.env.NEXT_PUBLIC_MAP_ADI,
+//   },
+// };
+//
+// export default withNextIntl(nextConfig);
