7cc152e73026dfa1e5a80307c82162ea3ce96ddd
Babenko Liudmyla <savelieva0509@gmail.com>
2024-09-19 16:34:31 +0300

    adaptive full calendar

--- src/components/admin/Booking/components/Calendar.tsx
+++ src/components/admin/Booking/components/Calendar.tsx
@@ -4,45 +4,47 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-// import NewEventModal from "./AddModal";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
+import dayjs, { Dayjs } from "dayjs";
+import AddModal from "./AddModal";
 // import EditEventModal from "./EditModal";
 
 type CalendarEvent = {
   title: string;
-  start: Date;
-  end: Date;
+  start: Dayjs; 
+  end: Dayjs;
   desc?: string;
+  resourceId?: string;
 };
+
+const resources = [
+  { id: "1", title: "Номер 101" },
+  { id: "2", title: "Номер 102" },
+  { id: "3", title: "Номер 103" },
+];
 
 const MyCalendar: React.FC = () => {
   const [events, setEvents] = useState<CalendarEvent[]>([]);
   const [currentEvent, setCurrentEvent] = useState<CalendarEvent | null>(null);
-  const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false);
-  const [isEditEventModalOpen, setIsEditEventModalOpen] = useState(false);
+  const [isNewBookModalOpen, setIsNewBookModalOpen] = useState(false);
 
   const handleCloseModals = () => {
-    setIsNewEventModalOpen(false);
-    setIsEditEventModalOpen(false);
+    setIsNewBookModalOpen(false);
   };
 
-  const handleSlotSelected = (info: any) => {
-    console.log(info);
-    setCurrentEvent({
-      title: "",
-      start: info.start,
-      end: info.end,
-      desc: "",
-    });
-    setIsNewEventModalOpen(true);
-  };
+ const handleSlotSelected = (info: any) => {
+   setCurrentEvent({
+     title: "",
+     start: dayjs(info.start), 
+     end: dayjs(info.end), 
+     resourceId: info.resource.id,
+   });
+   setIsNewBookModalOpen(true);
+ };
 
-  const handleEventSelected = (event: CalendarEvent) => {
-    setCurrentEvent(event);
-    setIsEditEventModalOpen(true);
-  };
-
-  const addNewEvent = (newEvent: CalendarEvent) => {
+  const addNewBook = (newEvent: CalendarEvent) => {
     setEvents([...events, newEvent]);
+    setIsNewBookModalOpen(false);
   };
 
   const updateExistingEvent = (updatedEvent: CalendarEvent) => {
@@ -53,28 +55,75 @@ const MyCalendar: React.FC = () => {
     );
   };
 
-  const deleteEvent = (start: Date) => {
-    setEvents(events.filter((event) => event.start !== start));
-  };
+  // const deleteEvent = (start: Date) => {
+  //   setEvents(events.filter((event) => event.start !== start));
+  // };
 
   return (
-    <div style={{ height: "100vh" }}>
+    <div className="container">
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          resourceTimelinePlugin,
+        ]}
+        initialView="resourceTimelineWeek"
+        events={events.map((event) => ({
+          ...event,
+          start: event.start.toDate(), // Преобразуем Dayjs в Date для FullCalendar
+          end: event.end.toDate(), // Преобразуем Dayjs в Date для FullCalendar
+        }))}
+        resources={resources}
         selectable={true}
         editable={true}
+        slotDuration={{ days: 1 }}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right:
+            "resourceTimelineWeek,resourceTimelineTwoWeeks,resourceTimelineMonth",
+        }}
+        buttonText={{
+          resourceTimelineWeek: "week",
+          resourceTimelineTwoWeeks: "two weeks",
+          resourceTimelineMonth: "month",
+        }}
+        views={{
+          resourceTimelineWeek: {
+            slotLabelFormat: [
+              { weekday: "short" },
+              { day: "numeric", month: "short" },
+            ],
+            resourcesInitiallyExpanded: true,
+          },
+          resourceTimelineTwoWeeks: {
+            type: "resourceTimeline",
+            duration: { weeks: 2 },
+            slotLabelFormat: [
+              { weekday: "short" },
+              { day: "numeric", month: "short" },
+            ],
+          },
+          resourceTimelineMonth: {
+            slotLabelFormat: [
+              { weekday: "short" },
+              { day: "numeric", month: "short" },
+            ],
+            resourcesInitiallyExpanded: true,
+          },
+        }}
+        select={handleSlotSelected}
       />
       Модалка для нового события
-      {/* {currentEvent && (
-        <NewEventModal
+      {currentEvent && (
+        <AddModal
           open={isNewEventModalOpen}
           handleClose={handleCloseModals}
           currentEvent={currentEvent}
           addNewEvent={addNewEvent}
         />
-      )} */}
+      )}
       {/* Модалка для редактирования события */}
       {/* {currentEvent && (
         <EditEventModal
