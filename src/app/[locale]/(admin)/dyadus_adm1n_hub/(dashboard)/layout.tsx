46b609bd210daeae7953e0794341dbd4bb44ca2d
Homer <Home@fdasdar>
2024-10-02 23:25:41 +0300

    added dynamic data for houses and gallery menus

--- src/app/[locale]/(admin)/dyadus_adm1n_hub/(dashboard)/layout.tsx
+++ src/app/[locale]/(admin)/dyadus_adm1n_hub/(dashboard)/layout.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import Header from '@/components/Admin/Header/Header'
 import MainMenu from '@/components/Admin/MainMenu/MainMenu';
 
-import { menuAdmin } from '@/data/admin/menu';
 import { verifySession } from '@/functions/dal';
+import { getData } from '@/actions/getData';
+import getMainMenu from '@/functions/getMainMenu';
 
-type Props = { children: React.ReactNode; }
+type Props = {
+  children: React.ReactNode;
+  params: { locale: string };
+ }
 
-export default async function DashboardLayout({ children }: Props) {
+export default async function DashboardLayout({ children, params: { locale } }: Props) {
   
   const session = await verifySession();
+  const houses = await getData<HouseItem[]>("houses");
+  const gallery = (await getData<GalleryItem[]>("gallery")).filter(gal => gal.name);
+  
+  const menuAdmin = getMainMenu(houses, gallery, locale);
+  
   const menu = session?.role === 'superadmin' ? menuAdmin : menuAdmin.filter((item) => session?.role === item.admission);
-
+  
   return (
+
     <>
       <Header />
       <div className="mx-auto flex">
