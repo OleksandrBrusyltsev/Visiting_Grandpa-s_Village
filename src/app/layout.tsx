5cf5b0414e31849cb8053ce519c805a366d17fe8
olegtkach <olegontkach101@gmail.com>
2024-10-30 16:03:08 +0200

    feat: google teg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,23 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
+import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/next'
+import {GoogleTagManager} from "@/components/google-tag-manager/google-tag-manager";
 
 type Props = {
   children: ReactNode;
 };
 
-export default function RootLayout({ children }: Props) {
-  return children;
+export default async function LocaleLayout({
+                                             children,
+                                           }: {
+  children: ReactNode
+}) {
+  return (
+      <>
+        <SpeedInsights/>
+        <GoogleTagManager/>
+        {children}
+        <Analytics/>
+      </>
+  )
 }
