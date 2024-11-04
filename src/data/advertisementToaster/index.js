0f2fdb6ccc7f7c24b427b6f77552270f6ba7d797
Alex <brusiltsev1@gmail.com>
2024-11-04 19:20:37 +0200

    new advert

--- src/data/advertisementToaster/index.js
+++ src/data/advertisementToaster/index.js
@@ -2,14 +2,14 @@ export const toasters = [
     {
         id: 1,
         //допустимые виды разделителей (в любой комбинации): ./,-
-        start_date: '01.09.2024',
-        end_date: '31.10.2024', //включая последний день
+        start_date: '05.10.2024',
+        end_date: '01.08.2025', //включая последний день
         timeout: 60, // в минутах
         is_active: true,
         translations: {
-            ru: 'Дедушка советует! Забронируй любой домик на две и более ночи со  скидкой 10% с 15 сентября! Успей воспользоваться уникальным предложением',
-            en: 'Grandpa advises! Book any house for two or more nights with a 10% discount starting from September 15th! Take advantage of the unique offer',
-            uk: 'Дідусь радить! Забронюй будь-який будиночок від двох діб зі знижкою 10% з 15 вересня! Встигни скористатися унікальною пропозицією',
+            "ru": "Дедушка советует! Забронируй домик на новогодние праздники и встреть Новый год в уюте и комфорте!",
+            "en": "Grandpa advises! Book a house for the New Year holidays and celebrate in comfort and coziness!",
+            "uk": "Дідусь радить! Забронюй будиночок на новорічні свята та зустрінь Новий рік у затишку та комфорті!"
         },
     }
 ];
