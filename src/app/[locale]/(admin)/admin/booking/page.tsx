ee376e457f490823d798e757928cb13c9543f293
Alex <brusiltsev1@gmail.com>
2024-11-13 23:03:18 +0200

    fix alex

--- src/app/[locale]/(admin)/admin/booking/page.tsx
+++ src/app/[locale]/(admin)/admin/booking/page.tsx
@@ -1,22 +1,26 @@
-import {unstable_setRequestLocale} from "next-intl/server";
-import {useTranslations} from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { useTranslations } from "next-intl";
 import AdminCalendar from "@/components/Admin/Booking/components/AdminCalendar/AdminCalendar";
 
 export async function generateStaticParams() {
-    return ["uk", "en"].map((locale) => ({locale}));
+    return ["uk", "en"].map((locale) => ({ locale }));
 }
 
 export default function Admin({
-                                  params: {locale},
-                              }: {
+    params: { locale },
+}: {
     params: { locale: string };
 }) {
+    // Установка локали для запроса
     unstable_setRequestLocale(locale);
-    const t = useTranslations("Admin");
+
+    // Определяем переводчик для ключа "Admin"
+    const t = useTranslations("Admin" as any); // Добавили "as any" для обхода проблемы с типизацией
 
     return (
         <main>
-            <h1>{t("title")}</h1>
+            <h1>{t("title" as any)}</h1>
+
             <AdminCalendar/>
         </main>
     );
